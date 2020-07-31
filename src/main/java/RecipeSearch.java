ad2e95da2d763eda27480aa4172c52dacc8d02b8
StephyMunoz <58042439+StephyMunoz@users.noreply.github.com>
2020-07-31 17:38:33 -0500

    Add files via upload

--- src/main/java/RecipeSearch.java
+++ src/main/java/RecipeSearch.java
@@ -20,95 +20,101 @@ public class RecipeSearch {
                 recipes.add(line);
             }
 
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-
-        while (true) {
-            System.out.println("");
-            System.out.println("Commands:");
-            System.out.println("list - list the recipes");
-            System.out.println("stop - stop the program");
-            System.out.println("find name - searched recipes by name");
-            System.out.println("find cooking time - searches recipes by cooking time");
-            System.out.println("find ingredient - searches recipes by ingredient");
-            System.out.println("");
-            System.out.print("Enter command: ");
-            String command = scanner.nextLine();
-            //System.out.println("");
-            if (command.equals("stop")) {
-                break;
-            } else if (command.equals("list")) {
-                System.out.println("Recipes:");
-                System.out.print(recipes.get(0) + ", cooking time: " + recipes.get(1));
-                nameRecipes.add(recipes.get(0));
-                nameRecipes.add(recipes.get(1));
+            while (true) {
                 System.out.println("");
-                for (int i = 0; i < recipes.size(); i++) {
-
-                    if (recipes.get(i).equals("")) {
-                        nameRecipes.add(recipes.get(i + 1));
-                        nameRecipes.add(recipes.get(i + 2));
-                        System.out.print(recipes.get(i + 1) + ", cooking time: " + recipes.get(i + 2));
-                        System.out.println("");
-                    }
-                }
-
-            } else if (command.equals("find name")) {
-                System.out.print("Searched word: ");
-                String searchedWord = scanner.nextLine();
-                System.out.println();
-                System.out.println("Recipes:");
-                for (int i = 0; i < nameRecipes.size(); i++) {
-                    if (nameRecipes.get(i).contains(searchedWord)) {
-                        System.out.print(nameRecipes.get(i) + ", cooking time: " + nameRecipes.get(i + 1));
-                        System.out.println();
-                    }
-                }
-
-            } else if (command.equals("find cooking time")) {
-                System.out.print("Max cooking time: ");
-                int cookingTime = Integer.valueOf(scanner.nextLine());
-                System.out.println();
-                System.out.println("Recipes:");
-                int timeCook = Integer.valueOf(recipes.get(1));
-                if (timeCook <= cookingTime) {
+                System.out.println("Commands:");
+                System.out.println("list - list the recipes");
+                System.out.println("stop - stop the program");
+                System.out.println("find name - searched recipes by name");
+                System.out.println("find cooking time - searches recipes by cooking time");
+                System.out.println("find ingredient - searches recipes by ingredient");
+                System.out.println("");
+                System.out.print("Enter command: ");
+                String command = scanner.nextLine();
+                if (command.equals("stop")) {
+                    break;
+                } else if (command.equals("list")) {
+                    System.out.println("Recipes:");
                     System.out.print(recipes.get(0) + ", cooking time: " + recipes.get(1));
-                }
-                for (int i = 0; i < recipes.size(); i++) {
-                    if (recipes.get(i).equals("")) {
-                        if (Integer.valueOf(recipes.get(i + 2)) <= cookingTime) {
-                            System.out.print(recipes.get(i + 1) + ", cooking time: " + recipes.get(i + 2) + "\n");
+                    nameRecipes.add(recipes.get(0));
+                    nameRecipes.add(recipes.get(1));
+                    System.out.println("");
+                    for (int i = 0; i < recipes.size(); i++) {
+
+                        if (recipes.get(i).equals("")) {
+                            nameRecipes.add(recipes.get(i + 1));
+                            nameRecipes.add(recipes.get(i + 2));
+                            System.out.print(recipes.get(i + 1) + ", cooking time: " + recipes.get(i + 2));
+                            System.out.println("");
                         }
                     }
-                }
-            } else if (command.equals("find ingredient")) {
-                System.out.print("Ingredient: ");
-                String ingredient = scanner.nextLine();
-                ArrayList<String> recipesFind = new ArrayList<>();
-                int counter = 0;
-                String output = "";
-                System.out.println();
-                System.out.println("Recipes:");
-                for (int i = 0; i < recipes.size(); i++) {
-                    if (recipes.get(i).equals(ingredient)) {
-                        counter++;
-                        for (int j = i; j >= 0; j--) {
-                            if(recipes.get(j).matches("[0-9]+")){
-                                output += recipes.get(j - 1) + ", cooking time: " + recipes.get(j) + "\n";
-                                recipesFind.add(output);
+
+                } else if (command.equals("find name")) {
+                    System.out.print("Searched word: ");
+                    String searchedWord = scanner.nextLine();
+                    System.out.println();
+                    System.out.println("Recipes:");
+                    if (recipes.get(0).contains(searchedWord)) {
+                        System.out.print(recipes.get(0) + ", cooking time: " + recipes.get(1));
+                        System.out.println("");
+                    }
+
+                    for (int i = 0; i < recipes.size(); i++) {
+
+                        if (recipes.get(i).equals("")) {
+                            if (recipes.get(i + 1).contains(searchedWord)) {
+                                System.out.print(recipes.get(i + 1) + ", cooking time: " + recipes.get(i + 2));
+                                System.out.println("");
                             }
                         }
                     }
-                }
-                if(counter == 1){
-                    System.out.println(recipesFind.get(0));
-                } else if(counter == 2){
-                    System.out.println(recipesFind.get(1));
-                } else if(counter == 3){
-                    System.out.println(recipesFind.get(2));
+
+                } else if (command.equals("find cooking time")) {
+                    System.out.print("Max cooking time: ");
+                    int cookingTime = Integer.valueOf(scanner.nextLine());
+                    System.out.println();
+                    System.out.println("Recipes:");
+                    int timeCook = Integer.valueOf(recipes.get(1));
+                    if (timeCook <= cookingTime) {
+                        System.out.print(recipes.get(0) + ", cooking time: " + recipes.get(1)+ "\n");
+                    }
+                    for (int i = 0; i < recipes.size(); i++) {
+                        if (recipes.get(i).equals("")) {
+                            if (Integer.valueOf(recipes.get(i + 2)) <= cookingTime) {
+                                System.out.print(recipes.get(i + 1) + ", cooking time: " + recipes.get(i + 2) + "\n");
+                            }
+                        }
+                    }
+                } else if (command.equals("find ingredient")) {
+                    System.out.print("Ingredient: ");
+                    String ingredient = scanner.nextLine();
+                    ArrayList<String> recipesFind = new ArrayList<>();
+                    int counter = 0;
+                    String output = "";
+                    System.out.println();
+                    System.out.println("Recipes:");
+                    for (int i = 0; i < recipes.size(); i++) {
+                        if (recipes.get(i).equals(ingredient)) {
+                            counter++;
+                            for (int j = i; j >= 0; j--) {
+                                if (recipes.get(j).matches("[0-9]+")) {
+                                    output += recipes.get(j - 1) + ", cooking time: " + recipes.get(j) + "\n";
+                                    recipesFind.add(output);
+                                }
+                            }
+                        }
+                    }
+                    if (counter == 1) {
+                        System.out.println(recipesFind.get(0));
+                    } else if (counter == 2) {
+                        System.out.println(recipesFind.get(1));
+                    } else if (counter == 3) {
+                        System.out.println(recipesFind.get(2));
+                    }
                 }
             }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
